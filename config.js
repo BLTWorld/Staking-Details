85dbb5a88a4eb27c37a8a1e2d1d303517cb9a5f0
BLTWorld <101034172+BLTWorld@users.noreply.github.com>
2022-07-18 00:24:22 -0700

    create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const NFTCONTRACT = "0x1f0dD278D62868fa58016060a1548973F8AFEd55";
+export const STAKINGCONTRACT = "0xF8C976C0c65853FE1db21E43056Fa936B88F4Be6";
+export const sonarCashApi = "https://testnet.sonar.cash/api";
+export const nftpng = "ipfs://QmSifFzarzzen5Vv4TWWhpN56VksqZrF3Bmuuc4gdGTEv1/";
+
+// token contract: 0xA85430d46863cea03fFC94c781962CE428b8eda6;
